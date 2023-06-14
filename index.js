53978c45d809023434908e28a958f3953b4c0726
Mona <2829922e@student.gla.ac.uk>
2023-06-14 10:35:43 +0100

    js paragraph edits

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ console.log("hello world");
 // window.alert("Attention!");
 
 let age = 26;
-console.log(age);
 
 let firstName = "Mona";
 let lastName = "Essaid";
-console.log("My name is " + firstName + " " + lastName);
 
+let student = true;
+
+document.getElementById("p1").innerHTML = "Hello " + firstName + "!";
+document.getElementById("p2").innerHTML = "You are " + age + " years old.";
+document.getElementById("p3").innerHTML = "Student? " + student;
+
+console.log(age);
+console.log("My name is ", firstName, lastName);
+console.log("Is", firstName, lastName, "a student? ", student);
