10466066b082c9c9f5a9b2548d41f156ac17c6a4
Mona <2829922e@student.gla.ac.uk>
2023-06-14 09:57:42 +0100

    jsfile

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+// window.alert("Attention!");
+
+let age = 26;
+console.log(age);
+
+let firstName = "Mona";
+let lastName = "Essaid";
+console.log("My name is " + firstName + " " + lastName);
+
